3381d15304d5d67cd648761b924bab0a1163e929
Raul Alfredo Chin Caamal <raul.chim@macropay.mx>
2024-09-09 16:52:13 -0600

    feat: add `back` option to `generateTemplateFiles` functions

--- src/tools/generate.js
+++ src/tools/generate.js
@@ -21,6 +21,22 @@ generateTemplateFiles([
     },
   },
   {
+    option: "Back",
+    defaultCase: CaseConverterEnum.PascalCase,
+    entry: {
+      folderPath: "./templates/back-service/",
+    },
+    stringReplacers: [{ question: "Service name", slot: "__name__" }],
+    output: {
+      path: "../../src",
+      pathAndFileNameDefaultCase: CaseConverterEnum.PascalCase,
+      overwrite: true,
+    },
+    onComplete: results => {
+      console.log(`results`, results)
+    },
+  },
+  {
     option: "BFF",
     defaultCase: CaseConverterEnum.PascalCase,
     entry: {

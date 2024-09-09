8f6083518d63f8639045bacd3d4ecb2585a50495
Raul Alfredo Chin Caamal <raul.chim@macropay.mx>
2024-09-09 09:57:55 -0600

    refactor: rename URL service name

--- src/tools/templates/front-service/constants/request-paths.ts
+++ src/tools/templates/front-service/constants/request-paths.ts
@@ -1,6 +1,6 @@
 import { BASE_PATH } from '../environments';
 
-export const URL___name__ = {
+export const URL___name__ConstantCase__ = {
   FRONT: `${BASE_PATH}/api/__name__Service`,
 };
 
--- src/tools/templates/front-service/services/front/__name__(lowerCase)-service.ts
+++ src/tools/templates/front-service/services/front/__name__(lowerCase)-service.ts
@@ -3,14 +3,14 @@ import {
   I__name__FrontServiceParams,
   I__name__FrontServiceResponse,
 } from '../../interfaces/services/front/__name__';
-import { URL___name__ } from '../../constants/request-paths';
+import { URL___name__ConstantCase__ } from '../../constants/request-paths';
 
 class __name__Service extends FetchServiceFE<
   I__name__FrontServiceResponse,
   I__name__FrontServiceParams
 > {
   constructor() {
-    super(URL___name__.FRONT);
+    super(URL___name__ConstantCase__.FRONT);
   }
 }
 
